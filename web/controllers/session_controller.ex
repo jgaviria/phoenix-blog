9d1a399a6cf15cf584797c5f0ef0885ddc20fbe7
Juan Gaviria salazar <jgaviria@Juans-MBP.fios-router.home>
2016-08-28 10:42:02 -0400

    Fixing bug when submitting nil username or passwords

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -1,7 +1,7 @@
 defmodule Pxblog.SessionController do
   use Pxblog.Web, :controller
   alias Pxblog.User
-  import Comeonin.Bcrypt, only: [checkpw: 2]
+  import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
 
   plug :scrub_params, "user" when action in [:create]
 
@@ -9,30 +9,30 @@ defmodule Pxblog.SessionController do
     render conn, "new.html", changeset: User.changeset(%User{})
   end
 
-  def create(conn, %{"user" => user_params}) do
-      Repo.get_by(User, username: user_params["username"])
-      |> sign_in(user_params["password"], conn)
+  def create(conn, %{"user" => %{"username" => username, "password" => password}})
+  when not is_nil(username) and not is_nil(password) do
+    user = Repo.get_by(User, username: username)
+    sign_in(user, password, conn)
   end
 
-   defp sign_in(user, password, conn) when is_nil(user) do
-      conn
-      |> put_flash(:error, "Invalid username/password combination!")
-      |> redirect(to: page_path(conn, :index))
+  def create(conn, _) do
+    failed_login(conn)
+  end
+
+   defp sign_in(user, _password, conn) when is_nil(user) do
+     failed_login(conn)
    end
 
    defp sign_in(user, password, conn) do
-      if checkpw(password, user.password_digest) do
+     if checkpw(password, user.password_digest) do
        conn
        |> put_session(:current_user, %{id: user.id, username: user.username})
        |> put_flash(:info, "Sign in successful!")
        |> redirect(to: page_path(conn, :index))
      else
-       conn
-       |> put_session(:current_user, nil)
-       |> put_flash(:error, "Invalid username/password combination!")
-       |> redirect(to: page_path(conn, :index))
+       failed_login(conn)
      end
-   end
+    end
 
    def delete(conn, _params) do
      conn
@@ -40,4 +40,14 @@ defmodule Pxblog.SessionController do
      |> put_flash(:info, "Signed out successfully!")
      |> redirect(to: page_path(conn, :index))
    end
+
+   defp failed_login(conn) do
+     dummy_checkpw()
+     conn
+     |> put_session(:current_user, nil)
+     |> put_flash(:error, "Invalid username/password combination!")
+     |> redirect(to: page_path(conn, :index))
+     |> halt()
+   end
+
 end
